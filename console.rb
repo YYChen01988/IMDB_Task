b393490ba2dff1f31641ce7b70e333774444f521
yingying <chen.yingying.01988@gmail.com>
2018-08-02 14:38:34 +0100

    test pass save and delete_all functions

---
+++ console.rb
@@ -0,0 +1,24 @@
+require_relative('models/star.rb')
+require_relative('models/movie.rb')
+require_relative('models/casting.rb')
+
+require( 'pry-byebug')
+
+Casting.delete_all()
+Movie.delete_all()
+Star.delete_all()
+
+star1 = Star.new({'first_name' => 'Johnny', 'last_name' => 'Deep'})
+star1.save()
+star2 = Star.new({'first_name' => 'Tom', 'last_name' => 'Hank'})
+star2.save()
+
+movie1 = Movie.new({'title' => 'Dark Shadows', 'genre' => 'Comedy'})
+movie1.save()
+movie2 = Movie.new({'title' => 'Forrest Gump', 'genre' => 'Drama'})
+movie2.save()
+
+
+
+binding.pry
+nil
--- models/casting.rb
+++ models/casting.rb
@@ -1,3 +1,28 @@
+require_relative("../db/sql_runner.rb")
+
 class Casting
+  attr_reader :id
+  attr_accessor :fee, :movie_id, :star_id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @fee = options["fee"].to_i
+    @movie_id = options['movie_id'].to_i
+    @star_id = options ['star_id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO stars(fee, movie_id, star_id) VALUES ($1, $2, $3) RETURNING id"
+    values = [@fee, @movie_id, @star_id]
+    casting = SqlRunner.run(sql, values)[0]
+    @id = casting['id'].to_i
+  end
+
+
+  def self.delete_all()
+    sql = "DELETE FROM castings"
+    values = []
+    SqlRunner.run(sql, values)
+  end
 
 end
